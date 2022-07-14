584e4667020059ebee99f8026d10846ab520fa94
Ubuntu <ubuntu@ip-172-31-12-143.us-west-1.compute.internal>
2022-07-14 18:17:50 +0000

    yo que se

--- Noticias/Noticias/settings.py
+++ Noticias/Noticias/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'l!*zb=*p)ym+a48(ky%60)f$ec5h4++5&m8&^0#w3$ifx8_#mt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['+']
+ALLOWED_HOSTS = ['13.57.202.40']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
